bec7cea03a2dc140913f80f2fecd4e4690e48c54
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-07-10 17:33:53 +0700

    register

--- app_users/views.py
+++ app_users/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.http import HttpRequest
 # Create your views here.
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
+
+def register(request: HttpRequest):
+    # Post request
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Save the user to the database
+            # Log the user in
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+    else:
+        form = RegisterForm()
+
+    # Get request
+    context = {"form": form}
+    return render(request, "app_users/register.html", context)
